d5a6cd5a97bf1c4d457f50b72fa173bc392445e7
BirmiiiYo <avramenkoa773@gmail.com>
2022-06-16 17:19:59 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import illustration from './images/illustration-woman-online-desktop.svg';
+import iconArrow from './images/icon-arrow-down.svg';
+import bgImg from './images/bg-pattern-desktop.svg';
 
 function App() {
+  const questions = [
+    'How many team members can I invite?',
+    'What is the maximum file upload size?',
+    'How do I reset my password?',
+    'Can I cancel my subscription?',
+    'Do you provide additional support?',
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <div className="wrapper">
+          <div className="images">
+            <img className="main-img" src={illustration} />
+            <img className="bg-img" src={bgImg} />
+          </div>
+          <div>
+            <h1>FAQ</h1>
+            <ul>
+              {questions.map((name, index) => (
+                <li key={`${name}_${index}`}>
+                  <p>{name}</p>
+
+                  <img src={iconArrow} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
