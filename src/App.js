cd39aa5f24fc14a830447a1b0b600c91bcd4d835
BirmiiiYo <avramenkoa773@gmail.com>
2022-06-18 22:20:31 +0300

    answers and img

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import './App.css';
 import illustration from './images/illustration-woman-online-desktop.svg';
 import iconArrow from './images/icon-arrow-down.svg';
 import bgImg from './images/bg-pattern-desktop.svg';
+import themeSwitch from './images/theme.svg';
+import block from './images/illustration-box-desktop.svg';
+import React from 'react';
 
 function App() {
+  const [visibleAns, setVisibleAns] = React.useState(false);
+
   const questions = [
     'How many team members can I invite?',
     'What is the maximum file upload size?',
     'How do I reset my password?',
     'Can I cancel my subscription?',
     'Do you provide additional support?',
+  ];
+  const answers = [
+    'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.',
+    'No more than 2GB. All files in your account must fit your allotted storage space.',
+    'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.',
+    'Yes! Send us a message and we’ll process your request no questions asked.',
+    'Chat and email support is available 24/7. Phone lines are open during normal business hours.',
   ];
 
   return (
@@ -19,15 +31,23 @@ function App() {
           <div className="images">
             <img className="main-img" src={illustration} />
             <img className="bg-img" src={bgImg} />
+            <img className="img-block" src={block} />
           </div>
           <div>
-            <h1>FAQ</h1>
+            <div className="title">
+              <h1>FAQ</h1>
+              <img className="theme" src={themeSwitch} onClick={() => alert('zxc')} />
+            </div>
             <ul>
-              {questions.map((name, index) => (
-                <li key={`${name}_${index}`}>
-                  <p>{name}</p>
-
-                  <img src={iconArrow} />
+              {questions.map((question, index) => (
+                <li key={`${question}_${index}`}>
+                  <div className="question">
+                    <p>{question}</p>
+                    <button onClick={() => setVisibleAns(!visibleAns)}>
+                      <img src={iconArrow} />
+                    </button>
+                  </div>
+                  {visibleAns && <div className="answer">{answers[index]}</div>}
                 </li>
               ))}
             </ul>
