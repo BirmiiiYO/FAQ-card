fdff9d72c79f592ba431a73a7161cdc61bad5800
BirmiiiYo <avramenkoa773@gmail.com>
2022-06-19 17:33:04 +0300

    theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import './App.css';
+import './App.scss';
 import illustration from './images/illustration-woman-online-desktop.svg';
 import iconArrow from './images/icon-arrow-down.svg';
 import bgImg from './images/bg-pattern-desktop.svg';
 import themeSwitch from './images/theme.svg';
 import block from './images/illustration-box-desktop.svg';
 import React from 'react';
+import { useTheme } from './useTheme.js';
 
 function App() {
+  const { theme, setTheme } = useTheme();
+
+  const handleThemeClick = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
+
   const [visibleAns, setVisibleAns] = React.useState(false);
+
+  const [activeQuestion, setActiveQuestion] = React.useState(null);
 
   const questions = [
     'How many team members can I invite?',
@@ -36,14 +49,14 @@ function App() {
           <div>
             <div className="title">
               <h1>FAQ</h1>
-              <img className="theme" src={themeSwitch} onClick={() => alert('zxc')} />
+              <img className="theme" src={themeSwitch} onClick={() => handleThemeClick()} />
             </div>
             <ul>
               {questions.map((question, index) => (
-                <li key={`${question}_${index}`}>
-                  <div className="question">
-                    <p>{question}</p>
-                    <button onClick={() => setVisibleAns(!visibleAns)}>
+                <li onClick={() => setActiveQuestion(index)} key={`${question}_${index}`}>
+                  <div className="question" onClick={() => setVisibleAns(!visibleAns)}>
+                    <p className={activeQuestion === index ? 'active' : ''}>{question}</p>
+                    <button>
                       <img src={iconArrow} />
                     </button>
                   </div>
@@ -59,3 +72,51 @@ function App() {
 }
 
 export default App;
+
+// <ul>
+//               <li>
+//                 <div className="question" onClick={() => setActiveQuestion(0)}>
+//                   <p>{questions[0]}</p>
+//                   <button onClick={() => setVisibleAns(!visibleAns)}>
+//                     <img src={iconArrow} />
+//                   </button>
+//                 </div>
+//                 {visibleAns && <div className="answer">{answers[0]}</div>}
+//               </li>
+//               <li>
+//                 <div className="question">
+//                   <p>{questions[1]}</p>
+//                   <button onClick={() => setVisibleAns(!visibleAns)}>
+//                     <img src={iconArrow} />
+//                   </button>
+//                 </div>
+//                 {visibleAns && <div className="answer">{answers[1]}</div>}
+//               </li>
+//               <li>
+//                 <div className="question">
+//                   <p>{questions[2]}</p>
+//                   <button onClick={() => setVisibleAns(!visibleAns)}>
+//                     <img src={iconArrow} />
+//                   </button>
+//                 </div>
+//                 {visibleAns && <div className="answer">{answers[2]}</div>}
+//               </li>
+//               <li>
+//                 <div className="question">
+//                   <p>{questions[3]}</p>
+//                   <button onClick={() => setVisibleAns(!visibleAns)}>
+//                     <img src={iconArrow} />
+//                   </button>
+//                 </div>
+//                 {visibleAns && <div className="answer">{answers[3]}</div>}
+//               </li>
+//               <li>
+//                 <div className="question">
+//                   <p>{questions[4]}</p>
+//                   <button onClick={() => setVisibleAns(!visibleAns)}>
+//                     <img src={iconArrow} />
+//                   </button>
+//                 </div>
+//                 {visibleAns && <div className="answer">{answers[4]}</div>}
+//               </li>
+//             </ul>
